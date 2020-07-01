c666ed358e13537f7c65528cf173712e69ea9a2e
Mostafa <enmosavi@gmail.com>
2020-07-01 22:55:46 +0430

    Initial Data Layer

--- CleanArch/CleanArch.MVC/Startup.cs
+++ CleanArch/CleanArch.MVC/Startup.cs
@@ -12,6 +12,7 @@ using CleanArch.MVC.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using CleanArch.Infra.Data.Context;
 
 namespace CleanArch.MVC
 {
@@ -32,6 +33,9 @@ namespace CleanArch.MVC
                     Configuration.GetConnectionString("IdentityDbConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddDbContext<UniversityDbContext>(options =>
+               options.UseSqlServer(
+                   Configuration.GetConnectionString("UniversityDbConnection")));
             services.AddRazorPages();
         }
 
